6e2df73b5ede77e04aac7c3b8ef5620de013ebe2
CaioMoura-cripto <caio.am6@gmail.com>
2025-10-31 12:47:27 -0300

    sanity-live

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // ...all other settings
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.sanity.io",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
